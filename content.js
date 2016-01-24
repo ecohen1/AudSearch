e0a40e62f30df1aa7e4dfd7ed2c43e936eb4bf8e
Eli Cohen <elicohen2018@u.northwestern.edu>
2016-01-24 08:42:25 -0500

    Made certain changes and aded fucnt

---
+++ content.js
@@ -0,0 +1,244 @@
+document.documentElement.style.height = '10%';
+document.body.style.height = '2%';
+document.documentElement.style.width = '100%';
+document.body.style.width = '100%';
+
+var a = document.createElement( 'div' );
+var b = document.createElement( 'div' );
+var c = document.createElement( 'div' );
+var d = document.createElement( 'div' );
+var e = document.createElement( 'div' );
+var f = document.createElement( 'div' );
+var g = document.createElement( 'div' );
+var h = document.createElement( 'div' );
+var i = document.createElement( 'div' );
+var j = document.createElement( 'div' );
+var k = document.createElement( 'div' );
+var l = document.createElement( 'div' );
+var m = document.createElement( 'div' );
+var n = document.createElement( 'div' );
+var o = document.createElement( 'div' );
+var p = document.createElement( 'div' );
+var q = document.createElement( 'div' );
+var r = document.createElement( 'div' );
+var s = document.createElement( 'div' );
+
+
+
+ document.body.appendChild( a );
+
+//set attributes for div
+a.id = 'myDivId';
+a.style.position = 'absolute';
+a.style.top = '65%';
+a.style.left = '6.4%';
+a.style.width = '0.3%';
+a.style.height = '1%';
+a.style.backgroundColor = 'yellow';
+a.style.zIndex = "100000";
+
+
+document.body.appendChild( b );
+
+//set attributes for div
+b.id = 'myDivId';
+b.style.position = 'absolute';
+b.style.top = '65%';
+b.style.left = '11.1%';
+b.style.width = '0.3%';
+b.style.height = '1%';
+b.style.backgroundColor = 'yellow';
+b.style.zIndex = "100000";
+
+
+document.body.appendChild( c );
+
+//set attributes for div
+c.id = 'myDivId';
+c.style.position = 'absolute';
+c.style.top = '65%';
+c.style.left = '13%';
+c.style.width = '0.3%';
+c.style.height = '1%';
+c.style.backgroundColor = 'yellow';
+c.style.zIndex = "100000";
+
+document.body.appendChild( d );
+
+//set attributes for div
+d.id = 'myDivId';
+d.style.position = 'absolute';
+d.style.top = '65%';
+d.style.left = '17%';
+d.style.width = '0.3%';
+d.style.height = '1%';
+d.style.backgroundColor = 'yellow';
+d.style.zIndex = "100000";
+
+
+
+
+document.body.appendChild( e );
+
+//set attributes for div
+e.id = 'myDivId';
+e.style.position = 'absolute';
+e.style.top = '65%';
+e.style.left = '18.6%';
+e.style.width = '0.3%';
+e.style.height = '1%';
+e.style.backgroundColor = 'yellow';
+e.style.zIndex = "100000";
+
+document.body.appendChild( f );
+
+//set attributes for div
+
+f.id = 'myDivId';
+f.style.position = 'absolute';
+f.style.top = '65%';
+f.style.left = '20%';
+f.style.width = '0.3%';
+f.style.height = '1%';
+f.style.backgroundColor = 'yellow';
+f.style.zIndex = "100000";
+
+
+document.body.appendChild( g );
+g.id = 'myDivId';
+g.style.position = 'absolute';
+g.style.top = '65%';
+g.style.left = '26.6%';
+g.style.width = '0.3%';
+g.style.height = '1%';
+g.style.backgroundColor = 'yellow';
+g.style.zIndex = "100000";
+
+document.body.appendChild( h );
+h.id = 'myDivId';
+h.style.position = 'absolute';
+h.style.top = '65%';
+h.style.left = '30.2%';
+h.style.width = '0.3%';
+h.style.height = '1%';
+h.style.backgroundColor = 'yellow';
+h.style.zIndex = "100000";
+
+
+document.body.appendChild( i );
+i.id = 'myDivId';
+i.style.position = 'absolute';
+i.style.top = '65%';
+i.style.left = '37.2%';
+i.style.width = '0.3%';
+i.style.height = '1%';
+i.style.backgroundColor = 'yellow';
+i.style.zIndex = "100000";
+
+
+document.body.appendChild( j );
+j.id = 'myDivId';
+j.style.position = 'absolute';
+j.style.top = '65%';
+j.style.left = '39.4%';
+j.style.width = '0.3%';
+j.style.height = '1%';
+j.style.backgroundColor = 'yellow';
+j.style.zIndex = "100000";
+
+
+document.body.appendChild( k );
+k.id = 'myDivId';
+k.style.position = 'absolute';
+k.style.top = '65%';
+k.style.left = '41%';
+k.style.width = '0.3%';
+k.style.height = '1%';
+k.style.backgroundColor = 'yellow';
+k.style.zIndex = "100000";
+
+
+document.body.appendChild( l );
+l.id = 'myDivId';
+l.style.position = 'absolute';
+l.style.top = '65%';
+l.style.left = '42.2%';
+l.style.width = '0.3%';
+l.style.height = '1%';
+l.style.backgroundColor = 'yellow';
+l.style.zIndex = "100000";
+
+
+document.body.appendChild( m );
+m.id = 'myDivId';
+m.style.position = 'absolute';
+m.style.top = '65%';
+m.style.left = '44.3%';
+m.style.width = '0.3%';
+m.style.height = '1%';
+m.style.backgroundColor = 'yellow';
+m.style.zIndex = "100000";
+
+
+document.body.appendChild( n );
+n.id = 'myDivId';
+n.style.position = 'absolute';
+n.style.top = '65%';
+n.style.left = '48.3%';
+n.style.width = '0.3%';
+n.style.height = '1%';
+n.style.backgroundColor = 'yellow';
+n.style.zIndex = "100000";
+
+
+document.body.appendChild( o );
+o.id = 'myDivId';
+o.style.position = 'absolute';
+o.style.top = '65%';
+o.style.left = '50.5%';
+o.style.width = '0.3%';
+o.style.height = '1%';
+o.style.backgroundColor = 'yellow';
+o.style.zIndex = "100000";
+
+
+document.body.appendChild( p );
+p.id = 'myDivId';
+p.style.position = 'absolute';
+p.style.top = '65%';
+p.style.left = '53.3%';
+p.style.width = '0.3%';
+p.style.height = '1%';
+p.style.backgroundColor = 'yellow';
+p.style.zIndex = "100000";
+
+
+document.body.appendChild( q );
+q.id = 'myDivId';
+q.style.position = 'absolute';
+q.style.top = '65%';
+q.style.left = '56.4%';
+q.style.width = '0.3%';
+q.style.height = '1%';
+q.style.backgroundColor = 'yellow';
+q.style.zIndex = "100000";
+
+document.body.appendChild( r );
+r.id = 'myDivId';
+r.style.position = 'absolute';
+r.style.top = '65%';
+r.style.left = '57.4%';
+r.style.width = '0.3%';
+r.style.height = '1%';
+r.style.backgroundColor = 'yellow';
+r.style.zIndex = "100000";
+
+document.body.appendChild( s );
+s.id = 'myDivId';
+s.style.position = 'absolute';
+s.style.top = '65%';
+s.style.left = '60.9%';
+s.style.width = '0.3%';
+s.style.height = '1%';
+s.style.backgroundColor = 'yellow';
+s.style.zIndex = "100000";
